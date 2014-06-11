ad58258a8f4bcb03972efa7bf2a30baf2abb726d
nagachandra <nagachandra.kn89@gmail.com>
2014-06-11 12:23:16 +0000

    added code for edit,update and delete actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,6 +23,29 @@ class ArticlesController < ApplicationController
   end
   
   
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    
+    redirect_to articles_path
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    
+    if article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+    
+  end
+  
+  
   private
   def article_params
     params.require(:article).permit(:title,:text)
